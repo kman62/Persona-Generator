9089ad4d3d6c18117c166950641ea13f82104f62
kmotte <kmotte@gmail.com>
2023-03-14 21:18:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,30 @@ openai.api_key = st.secrets['secrets']['openai_key']
 
 # Define the function to generate the persona
 def generate_persona(niche, location):
-    # rest of the code
+    # Generate the persona using OpenAI's GPT-3 API
+    prompt = f"As an experienced digital marketing manager, you're tasked with creating a User persona for {niche} and {location}. Create the persona with the following details. DEMOGRAPHICS Name,Age,Occupation,Annual Income,Marital Status, Family Situation, Location USER DESCRIPTION: use the DEMOGRAPHICS data to create a user description PSYCHOGRAPHICS: Personal characteristics, Hobbies, Interests, Personal aspirations, Professional goals, Pains, Main challenges, Needs, Dreams SHOPPING BEHAVIORS: Budget, Shopping Frequency, Preferred channels, Online behavior, Search terms, Preferred brands, Triggers, Barriers. The output must be in a table format for each of the sections above. The table must have headings data points and answers. MUST Be in a table. Each row much have an answer."
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=3000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # Extract the persona data from the API response
+    persona = response.choices[0].text
+    persona = persona.split("\n")
+
+    # Format the persona data as a Pandas DataFrame
+    data = []
+    for line in persona:
+        if ":" in line:
+            key, value = line.split(":")
+            data.append([key.strip(), value.strip()])
+    df = pd.DataFrame(data, columns=["Data Points", "Answers"])
+
+    return df
 
 # Define the Streamlit app
 st.set_page_config(page_title="Persona Generator", page_icon=":smiley:")
@@ -33,3 +56,4 @@ if submit_button:
 
     st.header("SHOPPING BEHAVIORS")
     st.table(persona[persona["Data Points"].str.contains("Budget|Shopping Frequency|Preferred channels|Online behavior|Search terms|Preferred brands|Triggers|Barriers")])
+
